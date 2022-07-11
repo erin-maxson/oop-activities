5ca554792af4e19805928cb9432f476efcd8d7c5
erin-maxson <erin@cohatch.com>
2022-07-11 18:00:47 -0500

    set timeout

--- 10-Stu_Promise-All/Unsolved/index.js
+++ 10-Stu_Promise-All/Unsolved/index.js
@@ -6,6 +6,14 @@ const callAPI = (duration) =>
     setTimeout(() => {
       // TODO: If the duration is longer than maxDuration, reject() the promise
       // TODO: Otherwise resolve() the promise
+      if (duration > maxDuration) {
+        reject (new Error ('This request timed out'))
+      }
+      else {
+        setTimeout(() => {
+          resolve('Response received: #### ms');
+        }, time);
+      }
     }, duration);
   });
 
@@ -14,3 +22,10 @@ const promises = apiCallDurations.map((duration) => callAPI(duration));
 console.log('Promises array before the timeouts have finished: ', promises);
 
 // TODO: Use Promise.all() to capture when the array of promises has been resolved or if any of them were rejected
+Promise.all(apiCallDurations)  
+  .then((values) => {
+    console.log(`\nResponse received: #### ms`)
+    console.log(values)
+  })
+
+.catch((err) => new Error(err));
